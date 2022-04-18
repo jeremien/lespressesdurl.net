3d4f0d59cb83d37db29892357876f2658c1b05cc
Jérémie Nuel <jeremie.nuel@pm.me>
2022-04-18 13:36:38 +0200

    first

---
+++ components/content.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="content">
+        <p>Initié en 2019, les presses d&rsquo;URL rassemblent des publications expérimentales. Ce projet est maintenu par Jérémie Nuel.
+       </p>
+        <p><a href="mailto:jeremie@lespressesdurl.net">jeremie@lespressesdurl.net</a></p>
+    </div>
+</template>
---
+++ components/nav.vue
@@ -0,0 +1,3 @@
+<template>
+    <nav class="nav">Les presses d&rsquo;URL</nav>
+</template>
